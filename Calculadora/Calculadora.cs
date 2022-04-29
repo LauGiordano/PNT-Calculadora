522e893d6b745d294cfbcd9ecf5e7fc70225a03d
Laura Giordano <mlgiordano92@gmail.com>
2022-04-28 21:28:41 -0300

    Add project files.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Calculadora
+    {
+        public int sumar(int nro1, int nro2)
+        {
+            return nro1 + nro2;
+        }
+
+        public int restar(int nro1, int nro2)
+        {
+            if (nro1 >= nro2)
+            {
+                return nro1 - nro2;
+            }
+            return nro2 + nro1;
+        }
+
+        public int multiplicar(int nro1, int nro2)
+        {
+            return nro1 * nro2;
+        }
+
+        public int dividir(int nro1, int nro2)
+        {
+            if (nro2 != 0)
+            {
+                return nro1 / nro2;
+            }
+            Console.WriteLine("No es posibile dividir por 0");
+            return -1;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Random rand = new Random();
+            int nro1 = rand.Next(1,100);
+            int nro2 = rand.Next(1, 100);
+            int suma = calculadora.sumar(nro1, nro2);
+            int resta = calculadora.restar(nro1, nro2);
+            int multiplicacion = calculadora.multiplicar(nro1, nro2);
+            int division = calculadora.dividir(nro1, nro2);
+            int divisionCero = calculadora.dividir(nro1, 0);
+
+            Console.WriteLine($"El resultado de la división entre {nro1} y {0} es {divisionCero}");
+            Console.WriteLine($"El resultado de la suma entre {nro1} y {nro2} es {suma}");
+            Console.WriteLine($"El resultado de la resta entre {nro1} y {nro2} es {resta}");
+            Console.WriteLine($"El resultado de la multiplicación entre {nro1} y {nro2} es {multiplicacion}");
+            Console.WriteLine($"El resultado de la división entre {nro1} y {nro2} es {division}");
+
+        }
+    }
+}
+        
+
